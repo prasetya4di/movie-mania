b9915ed18be167413d1a0595110918bb9532be69
Pras <prasetyaarfanto@gmail.com>
2023-05-10 16:19:29 +0700

    Add movie_service.dart

---
+++ lib/data/source/network/service/movie_service.dart
@@ -0,0 +1,25 @@
+import 'package:chopper/chopper.dart';
+import 'package:movie_mania/data/entity/movie/movie.dart';
+import 'package:movie_mania/data/source/network/response/movie/movie_popular_response.dart';
+import 'package:movie_mania/data/source/network/response/movie/movie_recommendation_response.dart';
+import 'package:movie_mania/data/source/network/response/movie/now_playing_response.dart';
+
+part 'movie_service.chopper.dart';
+
+@ChopperApi(baseUrl: "/movie")
+abstract class MovieService extends ChopperService {
+  static MovieService create([ChopperClient? client]) => _$MovieService(client);
+
+  @Get(path: "/now_playing")
+  Future<Response<NowPlayingResponse>> getNowPlaying();
+
+  @Get(path: "/popular")
+  Future<Response<MoviePopularResponse>> getPopularMovie();
+
+  @Get(path: "/{movie_id}/recommendations")
+  Future<Response<MovieRecommendationResponse>> getMovieRecommendation(
+      @Path("movie_id") int movieId);
+
+  @Get(path: "/{movie_id}")
+  Future<Response<Movie>> getDetail(@Path("movie_id") int movieId);
+}
