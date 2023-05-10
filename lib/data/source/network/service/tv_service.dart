fed0f0ff736b3dcee3a56b6d5553ee149ea5ba06
Pras <prasetyaarfanto@gmail.com>
2023-05-10 16:21:52 +0700

    Add tv_service.dart

---
+++ lib/data/source/network/service/tv_service.dart
@@ -0,0 +1,25 @@
+import 'package:chopper/chopper.dart';
+import 'package:movie_mania/data/entity/tv/tv.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_on_the_air_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_popular_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_recommendation_response.dart';
+
+part 'tv_service.chopper.dart';
+
+@ChopperApi(baseUrl: "/tv")
+abstract class TvService extends ChopperService {
+  static TvService create([ChopperClient? client]) => _$TvService(client);
+
+  @Get(path: "/on_the_air")
+  Future<Response<TvOnTheAirResponse>> getTvOnTheAir();
+
+  @Get(path: "/popular")
+  Future<Response<TvPopularResponse>> getPopularTv();
+
+  @Get(path: "/{tv_id}/recommendations")
+  Future<Response<TvRecommendationResponse>> getTvRecommendation(
+      @Path("tv_id") int tvId);
+
+  @Get(path: "/{tv_id}")
+  Future<Response<Tv>> getDetail(@Path("tv_id") int tvId);
+}
