b6171eb8ffb517f773143b100362faecdc0d3b69
Pras <prasetyaarfanto@gmail.com>
2023-05-10 16:00:43 +0700

    Add all response parser to json type parser

--- lib/data/source/network/json_type_parser.dart
+++ lib/data/source/network/json_type_parser.dart
@@ -1,10 +1,26 @@
 import 'package:movie_mania/data/source/network/parser_exception.dart';
+import 'package:movie_mania/data/source/network/response/movie/movie_popular_response.dart';
+import 'package:movie_mania/data/source/network/response/movie/movie_recommendation_response.dart';
+import 'package:movie_mania/data/source/network/response/movie/movie_search_response.dart';
+import 'package:movie_mania/data/source/network/response/movie/now_playing_response.dart';
 import 'package:movie_mania/data/source/network/response/response_error.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_on_the_air_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_popular_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_recommendation_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_search_response.dart';
 
 typedef JsonFactory<T> = T Function(Map<String, dynamic> json);
 
 class JsonTypeParser {
   static const Map<Type, JsonFactory> factories = {
+    NowPlayingResponse: NowPlayingResponse.fromJson,
+    MoviePopularResponse: MoviePopularResponse.fromJson,
+    MovieRecommendationResponse: MovieRecommendationResponse.fromJson,
+    MovieSearchResponse: MovieSearchResponse.fromJson,
+    TvOnTheAirResponse: TvOnTheAirResponse.fromJson,
+    TvPopularResponse: TvPopularResponse.fromJson,
+    TvRecommendationResponse: TvRecommendationResponse.fromJson,
+    TvSearchResponse: TvSearchResponse.fromJson,
     ResponseError: ResponseError.fromJson
   };
 
