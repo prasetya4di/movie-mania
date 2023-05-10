c1b4e094457ad1dab903a6a5f88c02d168694e21
Pras <prasetyaarfanto@gmail.com>
2023-05-10 15:14:34 +0700

    Add chopper factory

---
+++ lib/data/source/network/json_type_parser.dart
@@ -0,0 +1,33 @@
+import 'package:movie_mania/data/source/network/parser_exception.dart';
+import 'package:movie_mania/data/source/network/response/response_error.dart';
+
+typedef JsonFactory<T> = T Function(Map<String, dynamic> json);
+
+class JsonTypeParser {
+  static const Map<Type, JsonFactory> factories = {
+    ResponseError: ResponseError.fromJson
+  };
+
+  static dynamic decode<T>(dynamic entity) {
+    if (entity is Iterable) return _decodeList<T>(entity);
+
+    if (entity is Map<String, dynamic>) return _decodeMap<T>(entity);
+
+    return entity;
+  }
+
+  static T _decodeMap<T>(Map<String, dynamic> values) {
+    final jsonFactory = factories[T];
+    if (jsonFactory == null || jsonFactory is! JsonFactory<T>) {
+      /// throw serializer not found error;
+      throw JsonFactoryNotFoundException(T.toString());
+    }
+
+    return jsonFactory(values);
+  }
+
+  static List<T> _decodeList<T>(Iterable values) => values
+      .where((dynamic v) => v != null)
+      .map((dynamic v) => decode<T>(v) as T)
+      .toList();
+}
---
+++ lib/data/source/network/response_converter.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:chopper/chopper.dart';
+
+import 'json_type_parser.dart';
+import 'response/response_error.dart';
+
+class ResponseConverter extends JsonConverter {
+  @override
+  FutureOr<Response<ResultType>> convertResponse<ResultType, Item>(
+      Response response) async {
+    final jsonRes = await super.convertResponse<dynamic, dynamic>(response);
+    if (jsonRes.body == null ||
+        (jsonRes.body is String && (jsonRes.body as String).isEmpty)) {
+      return jsonRes.copyWith(body: null);
+    }
+    final dynamic body = jsonRes.body!;
+    final dynamic decodedItem = JsonTypeParser.decode<Item>(body);
+    return jsonRes.copyWith<ResultType>(body: decodedItem as ResultType);
+  }
+
+  @override
+  FutureOr<Response> convertError<ResultType, Item>(Response response) async {
+    final jsonRes = await super.convertError(response);
+    final dynamic body = jsonRes.body;
+    final dynamic responseError = JsonTypeParser.decode<ResponseError>(body);
+    return jsonRes.copyWith<ResponseError>(body: responseError);
+  }
+}
