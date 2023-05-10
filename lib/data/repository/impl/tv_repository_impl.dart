435987ee4378a2c3adc860f870be66166d5f8b33
Pras <prasetyaarfanto@gmail.com>
2023-05-10 17:08:50 +0700

    Add tv repository

---
+++ lib/data/repository/impl/tv_repository_impl.dart
@@ -0,0 +1,53 @@
+import 'package:either_dart/either.dart';
+import 'package:movie_mania/data/entity/tv/tv.dart';
+import 'package:movie_mania/data/repository/tv_repository.dart';
+import 'package:movie_mania/data/source/network/response/response_error.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_on_the_air_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_popular_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_recommendation_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_search_response.dart';
+import 'package:movie_mania/data/source/network/service/search_service.dart';
+import 'package:movie_mania/data/source/network/service/tv_service.dart';
+
+class TvRepositoryImpl implements TvRepository {
+  final TvService _tvService;
+  final SearchService _searchService;
+
+  TvRepositoryImpl(this._tvService, this._searchService);
+
+  @override
+  Future<Either<ResponseError, Tv>> getDetail(int id) async {
+    final response = await _tvService.getDetail(id);
+    return Either.condLazy(response.isSuccessful,
+        () => response.error as ResponseError, () => response.body!);
+  }
+
+  @override
+  Future<Either<ResponseError, TvPopularResponse>> getPopularTv() async {
+    final response = await _tvService.getPopularTv();
+    return Either.condLazy(response.isSuccessful,
+        () => response.error as ResponseError, () => response.body!);
+  }
+
+  @override
+  Future<Either<ResponseError, TvOnTheAirResponse>> getTvOnTheAir() async {
+    final response = await _tvService.getTvOnTheAir();
+    return Either.condLazy(response.isSuccessful,
+        () => response.error as ResponseError, () => response.body!);
+  }
+
+  @override
+  Future<Either<ResponseError, TvRecommendationResponse>> getTvRecommendation(
+      int id) async {
+    final response = await _tvService.getTvRecommendation(id);
+    return Either.condLazy(response.isSuccessful,
+        () => response.error as ResponseError, () => response.body!);
+  }
+
+  @override
+  Future<Either<ResponseError, TvSearchResponse>> searchTv(String key) async {
+    final response = await _searchService.searchTv(key);
+    return Either.condLazy(response.isSuccessful,
+        () => response.error as ResponseError, () => response.body!);
+  }
+}
---
+++ lib/data/repository/tv_repository.dart
@@ -0,0 +1,20 @@
+import 'package:either_dart/either.dart';
+import 'package:movie_mania/data/entity/tv/tv.dart';
+import 'package:movie_mania/data/source/network/response/response_error.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_on_the_air_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_popular_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_recommendation_response.dart';
+import 'package:movie_mania/data/source/network/response/tv/tv_search_response.dart';
+
+abstract class TvRepository {
+  Future<Either<ResponseError, TvOnTheAirResponse>> getTvOnTheAir();
+
+  Future<Either<ResponseError, TvPopularResponse>> getPopularTv();
+
+  Future<Either<ResponseError, TvRecommendationResponse>> getTvRecommendation(
+      int id);
+
+  Future<Either<ResponseError, Tv>> getDetail(int id);
+
+  Future<Either<ResponseError, TvSearchResponse>> searchTv(String key);
+}
