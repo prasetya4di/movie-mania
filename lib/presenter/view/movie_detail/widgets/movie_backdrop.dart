55ad1402757e11651678e3554e4f40a65ae39da2
Pras <prasetyaarfanto@gmail.com>
2023-05-11 15:20:28 +0700

    Add movie backdrop widget

---
+++ lib/presenter/view/movie_detail/widgets/movie_backdrop.dart
@@ -0,0 +1,29 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class MovieBackdrop extends StatelessWidget {
+  final String backdropPath;
+
+  const MovieBackdrop({super.key, required this.backdropPath});
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: backdropPath,
+      cacheKey: backdropPath,
+      progressIndicatorBuilder: (ctx, _, progress) {
+        return Shimmer.fromColors(
+          baseColor: Colors.grey.shade500,
+          highlightColor: Colors.grey.shade300,
+          child: Container(
+            height: 230,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
