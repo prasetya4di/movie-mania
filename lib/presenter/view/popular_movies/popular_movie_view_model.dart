34600e6405f4c32478f30735a7dc71b1e8583cd7
Pras <prasetyaarfanto@gmail.com>
2023-05-11 08:50:48 +0700

    Add popular movie view model

---
+++ lib/presenter/view/popular_movies/popular_movie_view_model.dart
@@ -0,0 +1,41 @@
+import 'dart:collection';
+
+import 'package:movie_mania/domain/usecases/fetch_popular_movies.dart';
+import 'package:movie_mania/presenter/base/base_view_model.dart';
+import 'package:movie_mania/presenter/model/movie_item_view_data_model.dart';
+
+class PopularMovieViewModel extends BaseViewModel {
+  final FetchPopularMovies _fetchPopularMovies;
+
+  PopularMovieViewModel(this._fetchPopularMovies);
+
+  int _currentPage = 1;
+  int _totalPage = 1;
+  final UnmodifiableListView<MovieItemViewDataModel> _popularMovies =
+      UnmodifiableListView([]);
+
+  UnmodifiableListView<MovieItemViewDataModel> get popularMovies =>
+      _popularMovies;
+
+  fetchPopularMovies() {
+    _fetchPopularMovies(_currentPage).then((value) {
+      value.fold(
+        (failure) => super.setResponseError(failure),
+        (movies) {
+          _totalPage = movies.totalPages;
+          _popularMovies.addAll(
+              movies.data.map((e) => MovieItemViewDataModel.fromMovieItem(e)));
+        },
+      );
+    }).then((value) {
+      notifyListeners();
+    });
+  }
+
+  fetchMorePopularMovies() {
+    if (_currentPage == _totalPage) return;
+    _currentPage++;
+
+    fetchPopularMovies();
+  }
+}
